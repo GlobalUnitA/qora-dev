363a8a55870c93499ad89bf434fd97c9c18cccb0
aethirtest <zettapool101@gmail.com>
2025-06-30 11:23:33 +0900

    로그인 블레이드 테스트 25

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                                 <div class="d-flex align-items-center justify-content-center mb-4">
                                     <a class="text-white fw-normal pe-3" href="{{ route('register') }}">{{ __('auth.sign_up') }}</a>
                                     <a class="text-white fw-normal position-relative divider px-3" href="{{ route('account.request') }}">{{ __('auth.find_id') }}</a>
-                                    <a class="text-white fw-normal position-relative divider ps-3" href="{{ route('password.request') }}">{{ __('auth.find_password') }} (test 24)</a>
+                                    <a class="text-white fw-normal position-relative divider ps-3" href="{{ route('password.request') }}">{{ __('auth.find_password') }} (test 25)</a>
                                 </div>
                             </form>
                         </div>
