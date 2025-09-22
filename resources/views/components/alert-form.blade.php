57609f167d3c4868f087e171726f1d0f910cd795
Design <globalunitadesign@google.com>
2025-09-22 17:44:56 +0900

    components 버튼 수정

--- resources/views/components/alert-form.blade.php
+++ resources/views/components/alert-form.blade.php
@@ -5,9 +5,9 @@
                 <h5 class="modal-title text-body">{{ __('system.alert') }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
-            <div id="alertMessage" class="modal-body fs-5 px-4" id="confirmBtn"></div>
+            <div id="alertMessage" class="modal-body fs-5 px-4"></div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-components text-white border-0">{{ __('system.confirm') }}</button>
+                <button type="button" class="btn btn-components text-white border-0" id="confirmBtn">{{ __('system.confirm') }}</button>
             </div>
         </div>
     </div>
