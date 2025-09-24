3c788d2f4b6cc2131bff782e218b4d9f9c682ea0
aethierTest2 <cjo092783@gmail.com>
2025-09-24 17:56:38 +0900

    마이닝 정책 개편 - 3.5

--- resources/views/admin/income/level-list.blade.php
+++ resources/views/admin/income/level-list.blade.php
@@ -86,7 +86,7 @@
                                         <td scope="col" class="text-center">{{ $value->income->coin->name }}</td>
                                         <td scope="col" class="text-center">{{ $value->amount }}</td>
                                         <td scope="col" class="text-center">
-                                            @switch($value->levelBonus?->bonus->reward->type)
+                                            @switch($value->levelBonus?->reward->type)
                                                 @case('instant')
                                                     {{ __('즉시') }}
                                                     @break
