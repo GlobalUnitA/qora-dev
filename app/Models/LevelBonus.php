374edd10c9995d343d17776ed7423fb3af827aff
aethierTest2 <cjo092783@gmail.com>
2025-09-24 18:59:50 +0900

    관리자 레벨매칭 소수점 처리 - 2

--- app/Models/LevelBonus.php
+++ app/Models/LevelBonus.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\TruncatesDecimals;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LevelBonus extends Model
 {
-    use HasFactory;
+    use HasFactory, TruncatesDecimals;
 
     protected $fillable = [
         'user_id',
